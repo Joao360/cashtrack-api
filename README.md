cd724143e7b2c4e17880f7400abd2c27a8a70a4f
João Graça <jonymiguelgraca@gmail.com>
2019-09-14 14:24:25 +0100

    Add models

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CashTracker-API
 CashTracker's web API so you can track where your money is going
+
+Python version: 3.7.4
--- cashtrack/app/models.py
+++ cashtrack/app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+RECORD_TYPES = ['Income', 'Expense']
+CATEGORIES = ['FOOD', 'TECH', 'HOUSE', 'HEALTH', 'TAXES', 'TRANSPORTATION']
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Subcategory(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+class Record(models.Model):
+    recordType = models.CharField(choices=RECORD_TYPES, default='Income', max_length=64)
+    #Add relation to category
+    ammount = models.FloatField("ammount of cash")
+    category = models.CharField(max_length=64)
+    entity = models.CharField("the other entity in the tradeoff", max_length=64, blank=True)
+    datetime = models.DateTimeField(auto_now_add=True)
+    note = models.CharField(max_length=256, blank=True)
