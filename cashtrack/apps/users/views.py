bdf8011f32c4f78d63febf4c6de9c24ff1cdd560
João Graça <jonymiguelgraca@gmail.com>
2019-12-01 15:25:17 +0000

    Link user to record

--- cashtrack/apps/users/views.py
+++ cashtrack/apps/users/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from .serializers import UserSerializer
 from rest_framework.generics import ListAPIView, RetrieveAPIView
+
+from .serializers import UserSerializer
 
 
 class UserList(ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
 
 class UserDetail(RetrieveAPIView):
     queryset = User.objects.all()
