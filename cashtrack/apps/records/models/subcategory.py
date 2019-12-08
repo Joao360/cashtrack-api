b6230d205cd40ca995198a4286c37148747a4379
João Graça <jonymiguelgraca@gmail.com>
2019-12-08 13:15:23 +0000

    Refactor models

---
+++ cashtrack/apps/records/models/subcategory.py
@@ -0,0 +1,6 @@
+from django.db import models
+from .category import Category
+
+class Subcategory(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
