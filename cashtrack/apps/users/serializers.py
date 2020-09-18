fdb3ae92f5f209956dd51ba455e4cde032b9bf10
João Graça <jonymiguelgraca@gmail.com>
2020-09-18 19:42:06 +0100

    Add update to user serializer

--- cashtrack/apps/users/serializers.py
+++ cashtrack/apps/users/serializers.py
@@ -10,7 +10,12 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         user = super().create(validated_data)
         user.set_password(validated_data['password'])
         user.save()
+        return user
 
+    def update(self, instance, validated_data):
+        user = super().update(instance, validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
         return user
 
     class Meta:
